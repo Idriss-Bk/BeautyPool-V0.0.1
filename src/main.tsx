c72bbe42364f0580e5d6a48bffd9599500eadd0a
IDRISS BOUKDIR <150384920+Idriss-Bk@users.noreply.github.com>
2026-07-16 02:47:37 +0400

    remove font and recall page in main

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import Hero from './components/static/sections/Hero.tsx'
+import LiveCounter from './components/static/sections/liveCounter.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
     <Hero/>
+    <LiveCounter/>
   </StrictMode>,
 )
